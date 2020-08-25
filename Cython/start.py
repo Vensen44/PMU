441c0ada3af178bb95583d6ef96694ac1e93ef98
Vensen44 <67536449+Vensen44@users.noreply.github.com>
2020-08-25 22:24:14 +0800

    Update start.py

--- Cython/start.py
+++ Cython/start.py
@@ -4,12 +4,13 @@ import numpy as np
 '''
 Set sampling rate(fs_), frequency base(base_frequency), angle of signal(theta), frequency of signal(freq_) and you can begin.
 '''
-ndata_ = 750 # how many data you use to calculate to a frquecny , angle , voltage
+
 fs_ = 7500  # sample rate, must > base_frequency * 2
+ndata_ = int(fs_ * 0.1) # how many data you use to calculate to a frquecny , angle , voltage
 theta = 30 * np.pi / 180  # the angleof signal(voltage) you wnat to create(Degree to Radian)
 base_frequency = 60  # frequency base of country , area or you want to it be (Hz)
 freq_ = 59  # the frequency of signal(voltage) you want to creat (Hz)
-N_ = fs_/base_frequency  # N is the number of signal(voltage) data that is used to calculate a DFT value
+N_ = int(fs_/base_frequency)  # N is the number of signal(voltage) data that is used to calculate a DFT value
 t = np.arange(0,1,1.0/fs_)  # generating 0 to 1 second array per 1/fs_ second, [0, 1/fs, 2/fs, ...., 1]
 vi = np.cos(2*np.pi*freq_*t + theta)[0:ndata_].astype('complex128')
 # signal(voltage) array, you can add some harmonics or noises behinde vi array (type is complex)
