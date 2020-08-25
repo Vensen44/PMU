6a12d43da4141be7a271642bb594cde052e703d4
Vensen44 <67536449+Vensen44@users.noreply.github.com>
2020-08-25 20:45:32 +0800

    Update setup.py

--- Cython/setup.py
+++ Cython/setup.py
@@ -3,13 +3,13 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-ext_modules=[ Extension("SDFT_one",
-              ["SDFT_one.pyx"],
+ext_modules=[ Extension("SDFT",
+              ["SDFT.pyx"],
               libraries=["bcm2835","m"],
               extra_compile_args = ["-ffast-math"])]
 
 setup(
-  name = "SDFT_one",
+  name = "SDFT",
   cmdclass = {"build_ext": build_ext},
   ext_modules = ext_modules,
   include_dirs=[numpy.get_include()])
