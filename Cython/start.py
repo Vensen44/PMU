9480c015186a22771980cf52e72978988d0581a7
Vensen44 <67536449+Vensen44@users.noreply.github.com>
2020-08-25 21:04:55 +0800

    Update start.py

--- Cython/start.py
+++ Cython/start.py
@@ -1,6 +1,9 @@
 import SDFT  # import your C file creation
 import numpy as np
 
+'''
+Set sampling rate(fs_), frequency base(base_frequency), angle of signal(theta), frequency of signal(freq_) and you can begin.
+'''
 ndata_ = 750 # how many data you use to calculate to a frquecny , angle , voltage
 fs_ = 7500  # sample rate, must > base_frequency * 2
 theta = 30 * np.pi / 180  # the angleof signal(voltage) you wnat to create(Degree to Radian)
@@ -13,7 +16,7 @@ vi = np.cos(2*np.pi*freq_*t + theta)[0:ndata_].astype('complex128')
 shift = 0  # shift points of vi array
 
 freq, Vrms, Vangle = SDFT.Smart_DFT(ndata_, fs_, N_, vi, shift, base_frequency)
-# call the function, Smart_DFT of the file, SDFT_one
+# call the function, Smart_DFT of the file, SDFT that cython created.
 print 'frequency =', freq, 'Hz'
 print 'magnitude of signal(voltage) in RMS value = ', Vrms, 'Volt'
 print 'angle of signal(voltage) in radian = ', Vangle, 'rad'
